10ce0eef8eceb720b5d67d6a2a4c54f497f189f8
Sourima Mukherjee <sourimamukherjee45@gmail.com>
2026-09-25 16:47:58 +0530

    Update Tailwind CSS configuration with custom colors
    Removed default text and font styles. Added Tailwind CSS configuration with custom brand colors.

--- tailwind.config.jsx
+++ tailwind.config.jsx
@@ -1,8 +1,23 @@
-.text-4xl {
-  font-size: 2.25rem;
-  line-height: 2.5rem;
-}
-
-.font-bold {
-  font-weight: 700;
-}
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: "#eefcf6",
+          100: "#d5f7e7",
+          200: "#aeefd2",
+          300: "#78e0b8",
+          400: "#42c99b",
+          500: "#1fae82",
+          600: "#148c69",
+          700: "#137056",
+          800: "#135946",
+          900: "#12493b",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
