4515b7fa51f432c523aae48c377a3ea806057e30
Sourima Mukherjee <sourimamukherjee45@gmail.com>
2026-09-25 17:02:51 +0530

    Create Note.js with Mongoose schema for notes

---
+++ Note.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const noteSchema = new mongoose.Schema(
+  {
+    patient: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, index: true },
+    doctor: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    content: { type: String, required: true, trim: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Note", noteSchema);
